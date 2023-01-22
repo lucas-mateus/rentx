06162e7753f5caa1feb333e657d061ff4dc8a2c3
lucas-mateus <xlucasmat@gmail.com>
2023-01-21 21:42:37 -0300

    feat: cria o header do projeto

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { StatusBar } from "react-native";
+import { Container, TotalCars, Header } from "./styles";
+import Logo from "../../assets/Logotipo.svg";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export function Home() {
   return (
     <Container>
-      <Title>Olá home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>Total de 112 carros</TotalCars>
+      </Header>
     </Container>
   );
 }
--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -1,12 +1,21 @@
 import styled from "styled-components/native";
-
+import { RFValue } from "react-native-responsive-fontsize";
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }) => theme.color.backgroundPrimary};
 `;
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.secondary_700};
+export const TotalCars = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.color.textDetail};
+  font-size: ${RFValue(15)}px;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 32px 24px 0;
+  background-color: ${({ theme }) => theme.color.header};
 `;
